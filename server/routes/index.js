27bed844561607a8adc0b583f177f0c49468a02e
Jinalkumar <jinalpatelalarsa@gmail.com>
2015-12-02 18:51:38 -0500

    final commit

---
+++ server/routes/index.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/user');
+var Businesscontact = require('../models/businesscontact');
+
+
+/* Render home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Home',
+        displayName: req.user ? req.user.displayName : ''
+    });
+});
+
+/* Render Login page. */
+router.get('/login', function (req, res, next) {
+    if (!req.user) {
+        res.render('login', {
+            title: 'Login',
+            messages: req.flash('loginMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    }
+    else {
+        return res.redirect('/users');
+    }
+});
+
+/* Process the Login Request */
+router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/users',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+/* Show Registration Page */
+router.get('/register', function (req, res, next) {
+    if (!req.user) {
+        res.render('register', {
+            title: 'Register',
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+    }
+    else {
+        return res.redirect('/');
+    }
+});
+
+/* POST signup data. */
+router.post('/register', passport.authenticate('local-registration', {
+        successRedirect : '/users',
+    failureRedirect : '/register',
+    failureFlash : true
+}));
+
+
+/* Process Logout Request */
+router.get('/logout', function (req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+
+/* Get Business page */
+router.get('/businesscontact', function(req, res, next) {
+   res.render('businesscontact',{ 
+   title: 'Business Contacts',
+        displayName: req.user ? req.user.displayName : '' });
+});
+
+/* Get about page */
+router.get('/about', function(req, res, next) {
+   res.render('about', {
+        title: 'about',
+        displayName: req.user ? req.user.displayName : '' });
+});
+/*Get projects page*/
+
+router.get('/projects', function(req, res, next) {
+   res.render('projects',{
+    title: 'projects',
+        displayName: req.user ? req.user.displayName : '' });
+});
+
+/*Get services page*/
+
+router.get('/services', function(req, res, next) {
+   res.render('services',{ 
+    title: 'services',
+        displayName: req.user ? req.user.displayName : '' });
+});
+
+/*Get contact me form*/
+
+router.get('/contactMe', function(req, res, next) {
+   res.render('contactMe',{ 
+    title: 'contactMe',
+        displayName: req.user ? req.user.displayName : '' });
+});
+
+/* Show Todo List Page */
+router.get('/todolist', function (req, res, next) {
+
+        res.render('todolist', {
+            title: 'Todos',
+            displayName: req.user ? req.user.displayName : '',
+            username: req.user ? req.user.username : '' 
+        });
+ 
+});
+
+
+
+
+module.exports = router;
